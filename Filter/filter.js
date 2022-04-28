f8bc817e24ef0e81794b2778c776e6afdb531029
Ivan-Lepeshinsky <clo-by@mail.ru>
2022-04-28 08:00:13 +0300

    edit component

--- Filter/filter.js
+++ Filter/filter.js
@@ -5,11 +5,57 @@ let filterComponent = React.createClass({
     testArr: React.PropTypes.array.isRequired,
   },
 
+  getInitialState: function () {
+    return {
+      startArr: this.props.testArr,
+      currentArr: this.props.testArr,
+      unsortArr: null,
+      alphabet: false,
+    };
+  },
+
+  letterSort: function (EO) {
+    let sampl = EO.target.value;
+    let arr = this.state.startArr.filter((word) => {
+      if (word.includes(sampl)) return word;
+    });
+    this.setState({ currentArr: arr });
+  },
+
+  alphabetSort: function () {
+    if (this.state.alphabet == false) {
+      this.setState((prevState, props) => {
+        return { unsortArr: prevState.currentArr };
+      });
+      this.setState((prevState, props) => {
+        return { alphabet: (prevState.alphabet = true) };
+      });
+      this.setState((prevState, props) => {
+        return { currentArr: prevState.currentArr.sort() };
+      });
+    } else {
+      this.setState((prevState, props) => {
+        return { alphabet: (prevState.alphabet = false) };
+      });
+      this.setState((prevState, props) => {
+        return { currentArr: prevState.unsortArr };
+      });
+    }
+  },
+
   render: function () {
-    let checkBox = React.DOM.input({ type: "checkbox", id: "check" });
-    let input = React.DOM.input({ id: "input" });
+    let checkBox = React.DOM.input({
+      type: "checkbox",
+      id: "check",
+      defaultChecked: false,
+      onChange: this.alphabetSort,
+    });
+    let input = React.DOM.input({ id: "input", onChange: this.letterSort });
     let button = React.DOM.button({ id: "button" }, "сброс");
-    let textArea = React.DOM.textarea({ id: "textarea" });
+    let textArea = React.DOM.textarea({
+      id: "textarea",
+      value: this.state.currentArr.join("\n"),
+    });
 
     return React.DOM.div(
       { className: "filter" },
